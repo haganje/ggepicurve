a7ea3c77453d34eb7b51f3dc0eae40107cac1a5d
jomuller <jomuller@jom.link>
2017-07-21 11:33:56 +0200

    refactoring and generalizing

--- R/fct_prepare_donnees.R
+++ R/fct_prepare_donnees.R
@@ -1,20 +1,22 @@
 #' Create an object df_epidemic
 #'
 #' @param dates Vector of dates
-#' @param group_color Vector of character
-#' @param groupe_facet Vector of character
-#' @param period_limit A character vector of size 1. Either "day", "week" (default) or "month".
 #' @param id Vector of character. The id of each obs. If NULL, then id will be autoassigned in the order of the dates.
+#' @param group_color Vector of character
+#' @param group_facet_vertical By default NA.
+#' @param group_facet_horizontal By default NA.
+#' @param period_limit A character vector of size 1. Either "day", "week" (default) or "month".
 #'
 #' @return A data.frame
 #' @export
 
 prepare_df <- function(
   dates,
-  group_color,
-  groupe_facet = NA,
-  period_limit = "week",
-  id = NULL
+  id = NULL,
+  group_color = NA,
+  group_facet_vertical = "all",
+  group_facet_horizontal = "all",
+  period_limit = "week"
   ) {
 
   # Check input ----------------------------------------------------------
@@ -27,48 +29,59 @@ prepare_df <- function(
     )
 
   # Create id of not given
-  date_order <- rank(dates, ties.method = "first")
-
-  if(is.null(id)) id <- date_order
+  if(is.null(id)) id <- rank(dates, ties.method = "first")
 
   # Create an ordered data frame
   df <- data.frame(
     id,
-    date_order,
     dates,
-    group_color
+    group_color,
+    group_facet_horizontal,
+    group_facet_vertical
   )
 
+  # Assign a period for each obs ----------------------------------------------
+
+  # Instead of nested ifelse or case funtion, use a list a dictionnary
   time_period_formats <- list(
     "day"   = "%m-%d",
     "week"  = "%W"   ,
     "month" = "%m"
   )
 
+  # Get the proper format for strftime function
   period_format <- time_period_formats[[period_limit]]
 
+  # format with Year to avoid a same period on different year
   period_full_format <- paste0("%Y-", period_format)
 
+  # Get all the possible levels for the range of dates
   periods_levels <- create_all_period_levels(dates, period_full_format)
 
+  # Assign name of the period for each obs
   period_name <- strftime(x = dates ,format = period_full_format)
 
   # Ordered factor with all levels to plot correctly
   df$period <- ordered(period_name, levels = periods_levels)
 
-  df <- df[order(df$dates), ]
-
-  #TODO: Refactor code (as a function)
-  df$ordre_periode <- as.integer(unlist(aggregate(date_order ~ period, data = df, FUN = order)[["date_order"]]))
-
-  # Placeholder for faceting group
-  df$group_facet <- "All"
-  df$group_facet_rank <- df$ordre_periode
+  # Rank based on subgroups
+  df$rank <- rank_by(dates, df$period, group_facet_horizontal, group_facet_vertical)
 
   class(df) <- c(class(df), "epidemic_df")
 
   df
 }
+
+#' Rank by group
+#'
+#' @description Rank the observations (e.g. dates) for each group.
+#' @seealso This function is a wrapper around the \code{\link{rank}} and \code{\link{ave}} functions.
+#' @param x A vector that could be coerced to numeric
+#' @param ... Vectors of the same length than x, with groups
+#' @return A vector with integer
+rank_by <- function(x, ...){
+  ave(x = as.numeric(x), ... = ..., FUN = function(x) rank(x, ties.method = "first"))
+  }
 
 # For faceting, just stack prepare_df for each group (Keep prepare_df simple)
 
@@ -83,23 +96,34 @@ prepare_df <- function(
 #' df_outbreak <- prepare_df(dates = outbreak_sim$dates, group_color = outbreak_sim$yard)
 #' plot_ggcurve(df_outbreak)
 
-plot_ggcurve <- function(df){
+plot_ggcurve <- function(df, color_name = "", facet_scale = "fixed"){
+
   ggp <- ggplot(df) +
     aes(
       x = period,
       fill = group_color,
-      y = group_facet_rank,
+      y = rank,
       label = id
       ) +
     geom_label(show.legend = TRUE, vjust = "top", size = 4) +
     scale_y_continuous(
       name = "Nombre de cas\n par semaine",
-      limits = c(0, max(df$group_facet_rank)),
+      limits = c(0, max(df$rank)),
       minor_breaks = NULL
     ) +
     scale_x_discrete(drop = FALSE) +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-    facet_grid(group_facet~.)
+    scale_fill_discrete(name = color_name) +
+    theme(axis.text.x = element_text(angle = 90))
+
+  h_formula <- "."
+  v_formula <- "."
+
+  if(length(unique(df$group_facet_horizontal)) > 1) h_formula <- "group_facet_horizontal"
+  if(length(unique(df$group_facet_vertical)) > 1) v_formula <- "group_facet_vertical"
+  if(h_formula != "." | v_formula != ".") {
+    facet_formula <- as.formula(paste0(v_formula, "~", h_formula))
+    ggp <- ggp + facet_grid(facet_formula, scales = facet_scale)
+  }
 
   # TODO : ajouter l'année en trouvant à chaque fois le x mini pour chaque année
   ggp
--- test_script/test_dfcreate.R
+++ test_script/test_dfcreate.R
@@ -2,7 +2,12 @@ z %>%
 arrange(dates_evenements, groupe_facet, ordre_periode_facet) %>%
 select(dates_evenements,  periode, ordre_periode, groupe_facet, ordre_periode_facet)
 
-z <- prepare_df(dates = outbreak_sim$dates, groupe_couleur = outbreak_sim$yard, groupe_facet = outbreak_sim$type)
+z <- prepare_df(dates = outbreak_sim$dates, group_color = outbreak_sim$yard, group_facet_vertical = outbreak_sim$type)
+
+plot_ggcurve(z)
+
+z <- prepare_df(dates = outbreak_sim$dates, group_color = outbreak_sim$yard, group_facet_vertical = strftime(outbreak_sim$dates, format = "%Y"))
+plot_ggcurve(z, facet_scale = "free_x") + scale_x_discrete(name = "Semaine", drop = TRUE)
 
 prepare_df("aes")
 prepare_df(c(as.Date(c("2017-01-01", NA))))
